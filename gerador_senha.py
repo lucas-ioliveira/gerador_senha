9bda2c267c8d1a82710e5ad6de248717e5d4a955
f1redr4g0n <>
2022-12-17 11:29:24 -0300

    adicionado .gitignore, simplificações e tratamento ao input

--- gerador_senha.py
+++ gerador_senha.py
@@ -1,25 +1,39 @@
-#Gerador de senhas
+# Gerador de senhas
 
-#Importação das bibliotecas da aplicação.
+# Importação das bibliotecas da aplicação.
+
 import random
 from time import sleep
 
-#Variáveis (Todas tratadas como strings).
+# Variáveis (todas tratadas como strings).
+
 letras_minusculas = 'abcdefghijklmnopqrstuvwxyz'
-letras_maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+letras_maiusculas = letras_minusculas.upper()
 numeros = '0123456789'
 caracteres = '[]{}()*#;/,-_%'
 
 # Perguntando ao usuário o tamanho da senha.
-tamanho = input('Digite qual o tamanho da senha: ')
 
-#Convertendo para os cálculos
-tamanho_int = int(tamanho)
-length = tamanho_int
+# Qualquer valor considerado inválido é recusado, permitindo ao usuário
+# tentar inserir outro valor.
 
-#Gerando a senha com todos os caracteres
+# Valores inválidos são valores não numéricos ou menores que 8, que é
+# considerado um ótimo tamanho mínimo para senhas fortes.
+
+while (True):
+    try:
+        tamanho = int(input('Digite qual o tamanho da senha: '))
+        if tamanho < 8:
+            raise Exception
+        break
+    except:
+        print('Por favor, insira um valor inteiro maior ou igual a 8.')
+
+# Gerando a senha com todos os caracteres
+
 todos_caracteres = letras_minusculas + letras_maiusculas + numeros + caracteres
-senha = ''.join(random.sample(todos_caracteres,length))
+senha = ''.join(random.sample(todos_caracteres, tamanho))
 print('Gerando sua senha...')
 sleep(2)
-print(f'A sua senha é: {senha}')
+print(f'A sua senha é: {senha}.')
+
